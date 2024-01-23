3c7fc0d0ed3ebfb1a04886720217f6d119af2557
ben <tringuyenlexuan@gmail.com>
2024-01-23 14:26:04 +0700

    Chuoi Commit : login

--- app/src/main/java/com/cstrien/thi_trac_nghiem/MainActivity.java
+++ app/src/main/java/com/cstrien/thi_trac_nghiem/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Ben comment "Hom qua em tuyet lam"
     }
 }
