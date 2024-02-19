2e7353a8b16881e9b70409656723753fc75e2777
trien <trienl641@gmail.com>
2024-02-19 19:50:59 +0700

    trien login resgister

--- app/src/main/java/com/cstrien/thi_trac_nghiem/MainActivity.java
+++ app/src/main/java/com/cstrien/thi_trac_nghiem/MainActivity.java
@@ -3,12 +3,22 @@ package com.cstrien.thi_trac_nghiem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnDangNhap,btnDangKi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addControl();
+
+    }
+
+    private void addControl() {
+        btnDangNhap=findViewById(R.id.btnDangNhap);
+        btnDangKi=findViewById(R.id.btnDangKi);
     }
 }
